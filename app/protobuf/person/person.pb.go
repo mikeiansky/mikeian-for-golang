ac60f2bc3ba68df8ec51f53c36262202508d1af9
mikeian <zhouwenxiang@xiaofupay.com>
2025-09-04 19:58:46 +0800

    test protobuf;

--- app/protobuf/person/person.pb.go
+++ app/protobuf/person/person.pb.go
@@ -2,13 +2,15 @@
 // versions:
 // 	protoc-gen-go v1.36.8
 // 	protoc        v6.31.1
-// source: app/protobuf/person.proto
+// source: app/protobuf/person/person.proto
 
 package person
 
 import (
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
+	city "mikeian-for-golang/app/protobuf/city"
+	biz "mikeian-for-golang/internal/biz"
 	reflect "reflect"
 	sync "sync"
 	unsafe "unsafe"
@@ -26,13 +28,15 @@ type Person struct {
 	state         protoimpl.MessageState `protogen:"open.v1"`
 	Name          string                 `protobuf:"bytes,1,opt,name=name,proto3" json:"name,omitempty"`
 	Age           int32                  `protobuf:"varint,2,opt,name=age,proto3" json:"age,omitempty"`
+	City          *city.City             `protobuf:"bytes,3,opt,name=city,proto3" json:"city,omitempty"`
+	Biz           *biz.Biz               `protobuf:"bytes,4,opt,name=biz,proto3" json:"biz,omitempty"`
 	unknownFields protoimpl.UnknownFields
 	sizeCache     protoimpl.SizeCache
 }
 
 func (x *Person) Reset() {
 	*x = Person{}
-	mi := &file_app_protobuf_person_proto_msgTypes[0]
+	mi := &file_app_protobuf_person_person_proto_msgTypes[0]
 	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 	ms.StoreMessageInfo(mi)
 }
@@ -44,7 +48,7 @@ func (x *Person) String() string {
 func (*Person) ProtoMessage() {}
 
 func (x *Person) ProtoReflect() protoreflect.Message {
-	mi := &file_app_protobuf_person_proto_msgTypes[0]
+	mi := &file_app_protobuf_person_person_proto_msgTypes[0]
 	if x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -57,7 +61,7 @@ func (x *Person) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use Person.ProtoReflect.Descriptor instead.
 func (*Person) Descriptor() ([]byte, []int) {
-	return file_app_protobuf_person_proto_rawDescGZIP(), []int{0}
+	return file_app_protobuf_person_person_proto_rawDescGZIP(), []int{0}
 }
 
 func (x *Person) GetName() string {
@@ -74,59 +78,80 @@ func (x *Person) GetAge() int32 {
 	return 0
 }
 
-var File_app_protobuf_person_proto protoreflect.FileDescriptor
+func (x *Person) GetCity() *city.City {
+	if x != nil {
+		return x.City
+	}
+	return nil
+}
 
-const file_app_protobuf_person_proto_rawDesc = "" +
+func (x *Person) GetBiz() *biz.Biz {
+	if x != nil {
+		return x.Biz
+	}
+	return nil
+}
+
+var File_app_protobuf_person_person_proto protoreflect.FileDescriptor
+
+const file_app_protobuf_person_person_proto_rawDesc = "" +
 	"\n" +
-	"\x19app/protobuf/person.proto\x12\x06person\".\n" +
+	" app/protobuf/person/person.proto\x12\x06person\x1a\x1capp/protobuf/city/city.proto\x1a\x16internal/biz/biz.proto\"j\n" +
 	"\x06Person\x12\x12\n" +
 	"\x04name\x18\x01 \x01(\tR\x04name\x12\x10\n" +
-	"\x03age\x18\x02 \x01(\x05R\x03ageB\x15Z\x13app/protobuf/personb\x06proto3"
+	"\x03age\x18\x02 \x01(\x05R\x03age\x12\x1e\n" +
+	"\x04city\x18\x03 \x01(\v2\n" +
+	".city.CityR\x04city\x12\x1a\n" +
+	"\x03biz\x18\x04 \x01(\v2\b.biz.BizR\x03bizB(Z&mikeian-for-golang/app/protobuf/personb\x06proto3"
 
 var (
-	file_app_protobuf_person_proto_rawDescOnce sync.Once
-	file_app_protobuf_person_proto_rawDescData []byte
+	file_app_protobuf_person_person_proto_rawDescOnce sync.Once
+	file_app_protobuf_person_person_proto_rawDescData []byte
 )
 
-func file_app_protobuf_person_proto_rawDescGZIP() []byte {
-	file_app_protobuf_person_proto_rawDescOnce.Do(func() {
-		file_app_protobuf_person_proto_rawDescData = protoimpl.X.CompressGZIP(unsafe.Slice(unsafe.StringData(file_app_protobuf_person_proto_rawDesc), len(file_app_protobuf_person_proto_rawDesc)))
+func file_app_protobuf_person_person_proto_rawDescGZIP() []byte {
+	file_app_protobuf_person_person_proto_rawDescOnce.Do(func() {
+		file_app_protobuf_person_person_proto_rawDescData = protoimpl.X.CompressGZIP(unsafe.Slice(unsafe.StringData(file_app_protobuf_person_person_proto_rawDesc), len(file_app_protobuf_person_person_proto_rawDesc)))
 	})
-	return file_app_protobuf_person_proto_rawDescData
+	return file_app_protobuf_person_person_proto_rawDescData
 }
 
-var file_app_protobuf_person_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
-var file_app_protobuf_person_proto_goTypes = []any{
-	(*Person)(nil), // 0: person.Person
+var file_app_protobuf_person_person_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
+var file_app_protobuf_person_person_proto_goTypes = []any{
+	(*Person)(nil),    // 0: person.Person
+	(*city.City)(nil), // 1: city.City
+	(*biz.Biz)(nil),   // 2: biz.Biz
 }
-var file_app_protobuf_person_proto_depIdxs = []int32{
-	0, // [0:0] is the sub-list for method output_type
-	0, // [0:0] is the sub-list for method input_type
-	0, // [0:0] is the sub-list for extension type_name
-	0, // [0:0] is the sub-list for extension extendee
-	0, // [0:0] is the sub-list for field type_name
+var file_app_protobuf_person_person_proto_depIdxs = []int32{
+	1, // 0: person.Person.city:type_name -> city.City
+	2, // 1: person.Person.biz:type_name -> biz.Biz
+	2, // [2:2] is the sub-list for method output_type
+	2, // [2:2] is the sub-list for method input_type
+	2, // [2:2] is the sub-list for extension type_name
+	2, // [2:2] is the sub-list for extension extendee
+	0, // [0:2] is the sub-list for field type_name
 }
 
-func init() { file_app_protobuf_person_proto_init() }
-func file_app_protobuf_person_proto_init() {
-	if File_app_protobuf_person_proto != nil {
+func init() { file_app_protobuf_person_person_proto_init() }
+func file_app_protobuf_person_person_proto_init() {
+	if File_app_protobuf_person_person_proto != nil {
 		return
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
-			RawDescriptor: unsafe.Slice(unsafe.StringData(file_app_protobuf_person_proto_rawDesc), len(file_app_protobuf_person_proto_rawDesc)),
+			RawDescriptor: unsafe.Slice(unsafe.StringData(file_app_protobuf_person_person_proto_rawDesc), len(file_app_protobuf_person_person_proto_rawDesc)),
 			NumEnums:      0,
 			NumMessages:   1,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
-		GoTypes:           file_app_protobuf_person_proto_goTypes,
-		DependencyIndexes: file_app_protobuf_person_proto_depIdxs,
-		MessageInfos:      file_app_protobuf_person_proto_msgTypes,
+		GoTypes:           file_app_protobuf_person_person_proto_goTypes,
+		DependencyIndexes: file_app_protobuf_person_person_proto_depIdxs,
+		MessageInfos:      file_app_protobuf_person_person_proto_msgTypes,
 	}.Build()
-	File_app_protobuf_person_proto = out.File
-	file_app_protobuf_person_proto_goTypes = nil
-	file_app_protobuf_person_proto_depIdxs = nil
+	File_app_protobuf_person_person_proto = out.File
+	file_app_protobuf_person_person_proto_goTypes = nil
+	file_app_protobuf_person_person_proto_depIdxs = nil
 }
--- app/protobuf/protobuf.go
+++ app/protobuf/protobuf.go
@@ -2,17 +2,28 @@ package main
 
 import (
 	"fmt"
+	"mikeian-for-golang/app/protobuf/city"
 	"mikeian-for-golang/app/protobuf/person"
 )
 
 func main() {
+
 	fmt.Println("app start ... ")
+
+	c := city.City{
+		Name:    "shenzhen",
+		Address: "guangdongshen nanshanqu",
+	}
+
+	fmt.Println(c)
 
 	p := person.Person{
 		Name: "mikeian",
-		Age:  33,
+		Age:  20,
+		City: &c,
 	}
 	fmt.Println(p)
 
 	fmt.Println("app complete ... ")
+
 }
