df3fbe989635523a88e371149eb863d06ec908e7
mike <929566854@qq.com>
2025-10-24 09:57:59 +0800

    test console log

---
+++ app/logs/console/console.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	fmt.Println("test log start ...")
+
+	fmt.Println("test println")
+	log.Println("test log 1")
+	log.Printf("test log 2")
+	// 这个打印致命错误，并且会退出程序
+	log.Fatalf("test log 3")
+	fmt.Println("test log complete ...")
+
+}
