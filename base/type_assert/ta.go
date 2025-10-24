e2a34a272334bca6ff5f45c5976488db3e601652
mike <929566854@qq.com>
2025-10-24 11:31:32 +0800

    test type assert;

--- base/type_assert/ta.go
+++ base/type_assert/ta.go
@@ -6,11 +6,42 @@ type IPerson interface {
 	SayHello()
 }
 
+func useNormalPerson(person IPerson) {
+	person.SayHello()
+}
+
+// 这里不能这样用
+//func usePointPerson(person *IPerson) {
+//	person.SayHello()
+//}
+
+type Animal interface {
+}
+
 type Normal struct {
+}
+
+type Temp struct {
 }
 
 func (n Normal) SayHello() {
 	fmt.Println("hello normal")
+}
+
+func TestTypeIsPerson(val interface{}) {
+	tn, ok := val.(IPerson)
+	fmt.Println("test type is IPerson", ok, tn)
+}
+
+func TestTypeIsTemp(val interface{}) {
+	tn, ok := val.(Temp)
+	fmt.Println("test type is temp", ok, tn)
+}
+
+func testTypeIsAnimal(val interface{}) {
+
+	tn, ok := val.(Temp)
+	fmt.Println("test type is animal", ok, tn)
 }
 
 func main() {
@@ -27,6 +58,26 @@ func main() {
 	nn, ok := np.(Normal)
 	fmt.Println(ok, nn)
 
+	pnn, pok := np.(*Normal)
+	fmt.Println(pok, pnn)
+
 	pn, ok := pp.(*Normal)
 	fmt.Println(ok, pn)
+
+	npn, pnok := pp.(Normal)
+	fmt.Println(pnok, npn)
+	fmt.Println("test type is temp")
+	TestTypeIsTemp(np)
+	TestTypeIsTemp(pp)
+
+	fmt.Println("test type is animal")
+	testTypeIsAnimal(np)
+	testTypeIsAnimal(pp)
+
+	fmt.Println("test type is person")
+	TestTypeIsPerson(np)
+	TestTypeIsPerson(pp)
+
+	useNormalPerson(np)
+	useNormalPerson(pp)
 }
