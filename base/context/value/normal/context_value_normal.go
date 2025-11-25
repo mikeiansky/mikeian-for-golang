049d7c70a570d6859088c2f3f35bb0e3fd9bc2b0
mike <929566854@qq.com>
2025-11-25 16:09:45 +0800

    test context body;

--- base/context/value/normal/context_value_normal.go
+++ base/context/value/normal/context_value_normal.go
@@ -5,10 +5,26 @@ import (
 	"fmt"
 )
 
+type ContextBody struct {
+	Id      int32
+	Name    string
+	Address string
+}
+
 func main() {
+
+	cb := ContextBody{
+		Id:      1,
+		Name:    "hello",
+		Address: "shenzhen",
+	}
+
+	fmt.Println("cb :", cb)
+
 	// 创建一个带有键值对的 context
 	ctx := context.WithValue(context.Background(), "userID", 123)
 	ctx = context.WithValue(ctx, "role", "admin")
+	ctx = context.WithValue(ctx, "body", &cb)
 
 	// 模拟一个函数使用这些值
 	printUserInfo(ctx)
@@ -17,6 +33,10 @@ func main() {
 func printUserInfo(ctx context.Context) {
 	userID := ctx.Value("userID")
 	role := ctx.Value("role")
-
-	fmt.Printf("用户ID: %v, 角色: %v\n", userID, role)
+	cb := ctx.Value("body")
+	ctb, _ := cb.(*ContextBody)
+	fmt.Println("body.Id: ", ctb.Id)
+	fmt.Println("body.Name: ", ctb.Name)
+	fmt.Println("body.Address: ", ctb.Address)
+	fmt.Printf("用户ID: %v, 角色: %v, body: %v\n", userID, role, cb)
 }
