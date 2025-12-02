78173d708e7a4206b46d9e374a612b853af73fed
mike <929566854@qq.com>
2025-12-02 16:37:02 +0800

    test rocketmq;

---
+++ app/mq/rocketmq/message/normal/sender/sender.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/apache/rocketmq-client-go/v2"
+	"github.com/apache/rocketmq-client-go/v2/primitive"
+	"github.com/apache/rocketmq-client-go/v2/producer"
+)
+
+func main() {
+	p, _ := rocketmq.NewProducer(
+		producer.WithNameServer([]string{"127.0.0.1:9876"}),
+		producer.WithRetry(2),
+	)
+
+	err := p.Start()
+	if err != nil {
+		fmt.Printf("start producer error: %s", err.Error())
+		return
+	}
+	defer p.Shutdown()
+
+	msg := &primitive.Message{
+		Topic: "TestTopic",
+		Body:  []byte("Hello RocketMQ Go Client 5!"),
+	}
+
+	res, err := p.SendSync(context.Background(), msg)
+	if err != nil {
+		fmt.Printf("send message error: %s\n", err)
+	} else {
+		fmt.Printf("send message success: result=%s\n", res.String())
+	}
+}
