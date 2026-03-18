590d4c8d40950a481b21e4eeeb968be0c756416c
mike <929566854@qq.com>
2026-03-18 15:41:09 +0800

    time test;

--- base/time/zone/zone.go
+++ base/time/zone/zone.go
@@ -8,9 +8,25 @@ import (
 func main() {
 
 	fmt.Println("time zone app start ... ")
+	now := time.Now()
 
-	fmt.Println("time.Now().UTC():", time.Now().UTC())
-	fmt.Println("time.Now():", time.Now())
+	fmt.Println("time.Now():", now)
+	fmt.Println("time.Now().UTC():", now)
+
+	nowStr := now.Format(time.DateTime)
+	fmt.Println("nowStr:", nowStr)
+
+	pt, _ := time.Parse(time.DateTime, nowStr)
+	fmt.Println("pt:", pt)
+
+	nowUtcStr := now.UTC().Format(time.DateTime)
+	fmt.Println("nowUtcStr:", nowUtcStr)
+
+	ptUtc, _ := time.Parse(time.DateTime, nowUtcStr)
+	fmt.Println("ptUtc:", ptUtc)
+
+	ptUtcUtc := ptUtc.UTC()
+	fmt.Println("ptUtcUtc:", ptUtcUtc)
 
 	fmt.Println("time zone app complete ... ")
 
