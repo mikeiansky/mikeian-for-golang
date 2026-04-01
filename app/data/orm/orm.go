536f8b200472eed48e3b4d2da59812a7d602ed7a
mike <929566854@qq.com>
2026-04-01 22:59:54 +0800

    test orm;

--- app/data/orm/orm.go
+++ app/data/orm/orm.go
@@ -41,16 +41,18 @@ func main() {
 	fmt.Println(p1, "p1.ID:", p1.ID)
 
 	//// Read
-	var product Product
-	db.First(&product, 2)                 // find product with integer primary key
-	db.First(&product, "code = ?", "F42") // find product with code D42
+	var product2 Product
+	db.First(&product2, 1) // find product with integer primary key
+	fmt.Println("find product2:", product2)
+
+	//db.First(&product, "code = ?", "F42") // find product with code D42
 	//
 	// Update - update product's price to 200
-	db.Model(&product).Update("Price", 300)
+	//db.Model(&product).Update("Price", 300)
 	//// Update - update multiple fields
-	db.Model(&product).Updates(Product{Price: 200, Code: "F42"}) // non-zero fields
-	db.Model(&product).Updates(map[string]interface{}{"Price": 200, "Code": "F42"})
+	//db.Model(&product).Updates(Product{Price: 200, Code: "F42"}) // non-zero fields
+	//db.Model(&product).Updates(map[string]interface{}{"Price": 200, "Code": "F42"})
 	//
 	// Delete - delete product
-	db.Delete(&product, 1)
+	//db.Delete(&product, 1)
 }
--- app/util/id.go
+++ app/util/id.go
@@ -2,18 +2,32 @@ package main
 
 import (
 	"fmt"
-	"log"
-
-	"github.com/google/uuid"
+	"strings"
 )
 
+func createSql(ids []int64) string {
+	//if len(ids) == 0 {
+	//	return []*core.Channel{}, nil
+	//}
+
+	placeholders := strings.TrimSuffix(strings.Repeat("?,", len(ids)), ",")
+	var channelQuery = fmt.Sprintf(`SELECT 
+         id, name, code, channel_type, image, currency, category, 
+         status, created_at_utc, updated_at_utc
+       FROM %s
+       WHERE id IN (%s)`, "channels", placeholders)
+	return channelQuery
+}
+
 func main() {
-	id, err := uuid.NewRandom()
-	if err != nil {
-		log.Fatal(err)
-	}
-	uuidString := id.String()
-	fmt.Println(uuidString)
+	//id, err := uuid.NewRandom()
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//uuidString := id.String()
+	//fmt.Println(uuidString)
 
 	//uuid.NewUUID()
+	ret := createSql([]int64{})
+	fmt.Println(ret)
 }
