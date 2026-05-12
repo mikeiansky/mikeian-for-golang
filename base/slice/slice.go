a704a46d01b38e0a4b51301db8d464301f219380
mike <929566854@qq.com>
2026-05-12 11:35:04 +0800

    test slice;

--- base/slice/slice.go
+++ base/slice/slice.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+func testNilSlice(s []int) {
+	fmt.Println("len(s):", len(s), ",cap(s):", cap(s))
+}
+
 func main() {
 
 	defer func() {
@@ -15,4 +19,10 @@ func main() {
 	//a[3] = 2 // 这样会报错
 	a = append(a, 1, 2, 3, 4, 5) // 这里用回增长，且不会受限于capacity
 	fmt.Println(a, len(a), cap(a))
+
+	var c []int
+	fmt.Println(c, len(c), cap(c))
+
+	testNilSlice(nil)
+
 }
