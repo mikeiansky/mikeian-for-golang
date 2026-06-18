d988a5775e553373355df0c1a3d025ef26fcff8e
mike <929566854@qq.com>
2026-06-18 15:02:13 +0800

    test time after;

--- base/init/init.go
+++ base/init/init.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"sync"
 	"time"
 )
 
@@ -19,10 +20,26 @@ func f() int {
 	return d
 }
 
+func hhh() {
+	sw := sync.WaitGroup{}
+	sw.Add(1)
+	var ne error
+	time.AfterFunc(1*time.Second, func() {
+		fmt.Println("after")
+		sw.Done()
+		ne = errors.New("time out")
+	})
+	sw.Wait()
+	//fmt.Println("after func ")
+	fmt.Println("after func ", ne.Error())
+}
+
 func main() {
 	fmt.Println(a, b, c, d)
 	errors.New("test")
 	ret := fmt.Sprintf("%d", time.Now().UnixMilli())
 	fmt.Println(ret)
-
+	hhh()
+	time.Sleep(2 * time.Second)
+	fmt.Println("complete ")
 }
